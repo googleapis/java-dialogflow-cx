79725a0acd67321c67c262a76aa4a205c91d345f
Jessie (Xiaoyu) Jiang <70290069+jessiexjiang27@users.noreply.github.com>
2020-08-30 23:54:13 -0400

    Fix package name
    Fix package format.

--- samples/snippets/src/main/java/com/example/dialogflow/cx/DetectIntent.java
+++ samples/snippets/src/main/java/com/example/dialogflow/cx/DetectIntent.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.dialogflow.cx;
 
 // [START dialogflow_cx_detect_intent_text]
