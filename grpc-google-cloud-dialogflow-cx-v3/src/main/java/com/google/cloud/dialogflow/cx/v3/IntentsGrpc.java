0e50fcb99c81ed3bc70c291b696a7ede52b71e14
yoshi-automation <yoshi-automation@google.com>
2020-11-24 16:39:37 -0800

    chore: Remove spurious build warnings by deleting--include_source_info from .bazelrc
    Committer: @miraleung
    PiperOrigin-RevId: 344134135
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Nov 24 14:38:21 2020 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: b11a87f3b54be3a5e42c15020ee1e0781f86da90
    Source-Link: https://github.com/googleapis/googleapis/commit/b11a87f3b54be3a5e42c15020ee1e0781f86da90

--- grpc-google-cloud-dialogflow-cx-v3/src/main/java/com/google/cloud/dialogflow/cx/v3/IntentsGrpc.java
+++ grpc-google-cloud-dialogflow-cx-v3/src/main/java/com/google/cloud/dialogflow/cx/v3/IntentsGrpc.java
@@ -22,13 +22,7 @@ import static io.grpc.stub.ClientCalls.futureUnaryCall;
 import static io.grpc.stub.ServerCalls.asyncUnaryCall;
 import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
 
-/**
- *
- *
- * <pre>
- * Service for managing [Intents][google.cloud.dialogflow.cx.v3.Intent].
- * </pre>
- */
+/** */
 @javax.annotation.Generated(
     value = "by gRPC proto compiler",
     comments = "Source: google/cloud/dialogflow/cx/v3/intent.proto")
@@ -302,22 +296,10 @@ public final class IntentsGrpc {
     return IntentsFutureStub.newStub(factory, channel);
   }
 
-  /**
-   *
-   *
-   * <pre>
-   * Service for managing [Intents][google.cloud.dialogflow.cx.v3.Intent].
-   * </pre>
-   */
+  /** */
   public abstract static class IntentsImplBase implements io.grpc.BindableService {
 
-    /**
-     *
-     *
-     * <pre>
-     * Returns the list of all intents in the specified agent.
-     * </pre>
-     */
+    /** */
     public void listIntents(
         com.google.cloud.dialogflow.cx.v3.ListIntentsRequest request,
         io.grpc.stub.StreamObserver<com.google.cloud.dialogflow.cx.v3.ListIntentsResponse>
@@ -325,52 +307,28 @@ public final class IntentsGrpc {
       asyncUnimplementedUnaryCall(getListIntentsMethod(), responseObserver);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Retrieves the specified intent.
-     * </pre>
-     */
+    /** */
     public void getIntent(
         com.google.cloud.dialogflow.cx.v3.GetIntentRequest request,
         io.grpc.stub.StreamObserver<com.google.cloud.dialogflow.cx.v3.Intent> responseObserver) {
       asyncUnimplementedUnaryCall(getGetIntentMethod(), responseObserver);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Creates an intent in the specified agent.
-     * </pre>
-     */
+    /** */
     public void createIntent(
         com.google.cloud.dialogflow.cx.v3.CreateIntentRequest request,
         io.grpc.stub.StreamObserver<com.google.cloud.dialogflow.cx.v3.Intent> responseObserver) {
       asyncUnimplementedUnaryCall(getCreateIntentMethod(), responseObserver);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Updates the specified intent.
-     * </pre>
-     */
+    /** */
     public void updateIntent(
         com.google.cloud.dialogflow.cx.v3.UpdateIntentRequest request,
         io.grpc.stub.StreamObserver<com.google.cloud.dialogflow.cx.v3.Intent> responseObserver) {
       asyncUnimplementedUnaryCall(getUpdateIntentMethod(), responseObserver);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Deletes the specified intent.
-     * </pre>
-     */
+    /** */
     public void deleteIntent(
         com.google.cloud.dialogflow.cx.v3.DeleteIntentRequest request,
         io.grpc.stub.StreamObserver<com.google.protobuf.Empty> responseObserver) {
@@ -415,13 +373,7 @@ public final class IntentsGrpc {
     }
   }
 
-  /**
-   *
-   *
-   * <pre>
-   * Service for managing [Intents][google.cloud.dialogflow.cx.v3.Intent].
-   * </pre>
-   */
+  /** */
   public static final class IntentsStub extends io.grpc.stub.AbstractAsyncStub<IntentsStub> {
     private IntentsStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
       super(channel, callOptions);
@@ -432,13 +384,7 @@ public final class IntentsGrpc {
       return new IntentsStub(channel, callOptions);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Returns the list of all intents in the specified agent.
-     * </pre>
-     */
+    /** */
     public void listIntents(
         com.google.cloud.dialogflow.cx.v3.ListIntentsRequest request,
         io.grpc.stub.StreamObserver<com.google.cloud.dialogflow.cx.v3.ListIntentsResponse>
@@ -449,13 +395,7 @@ public final class IntentsGrpc {
           responseObserver);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Retrieves the specified intent.
-     * </pre>
-     */
+    /** */
     public void getIntent(
         com.google.cloud.dialogflow.cx.v3.GetIntentRequest request,
         io.grpc.stub.StreamObserver<com.google.cloud.dialogflow.cx.v3.Intent> responseObserver) {
@@ -463,13 +403,7 @@ public final class IntentsGrpc {
           getChannel().newCall(getGetIntentMethod(), getCallOptions()), request, responseObserver);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Creates an intent in the specified agent.
-     * </pre>
-     */
+    /** */
     public void createIntent(
         com.google.cloud.dialogflow.cx.v3.CreateIntentRequest request,
         io.grpc.stub.StreamObserver<com.google.cloud.dialogflow.cx.v3.Intent> responseObserver) {
@@ -479,13 +413,7 @@ public final class IntentsGrpc {
           responseObserver);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Updates the specified intent.
-     * </pre>
-     */
+    /** */
     public void updateIntent(
         com.google.cloud.dialogflow.cx.v3.UpdateIntentRequest request,
         io.grpc.stub.StreamObserver<com.google.cloud.dialogflow.cx.v3.Intent> responseObserver) {
@@ -495,13 +423,7 @@ public final class IntentsGrpc {
           responseObserver);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Deletes the specified intent.
-     * </pre>
-     */
+    /** */
     public void deleteIntent(
         com.google.cloud.dialogflow.cx.v3.DeleteIntentRequest request,
         io.grpc.stub.StreamObserver<com.google.protobuf.Empty> responseObserver) {
@@ -512,13 +434,7 @@ public final class IntentsGrpc {
     }
   }
 
-  /**
-   *
-   *
-   * <pre>
-   * Service for managing [Intents][google.cloud.dialogflow.cx.v3.Intent].
-   * </pre>
-   */
+  /** */
   public static final class IntentsBlockingStub
       extends io.grpc.stub.AbstractBlockingStub<IntentsBlockingStub> {
     private IntentsBlockingStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
@@ -530,74 +446,38 @@ public final class IntentsGrpc {
       return new IntentsBlockingStub(channel, callOptions);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Returns the list of all intents in the specified agent.
-     * </pre>
-     */
+    /** */
     public com.google.cloud.dialogflow.cx.v3.ListIntentsResponse listIntents(
         com.google.cloud.dialogflow.cx.v3.ListIntentsRequest request) {
       return blockingUnaryCall(getChannel(), getListIntentsMethod(), getCallOptions(), request);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Retrieves the specified intent.
-     * </pre>
-     */
+    /** */
     public com.google.cloud.dialogflow.cx.v3.Intent getIntent(
         com.google.cloud.dialogflow.cx.v3.GetIntentRequest request) {
       return blockingUnaryCall(getChannel(), getGetIntentMethod(), getCallOptions(), request);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Creates an intent in the specified agent.
-     * </pre>
-     */
+    /** */
     public com.google.cloud.dialogflow.cx.v3.Intent createIntent(
         com.google.cloud.dialogflow.cx.v3.CreateIntentRequest request) {
       return blockingUnaryCall(getChannel(), getCreateIntentMethod(), getCallOptions(), request);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Updates the specified intent.
-     * </pre>
-     */
+    /** */
     public com.google.cloud.dialogflow.cx.v3.Intent updateIntent(
         com.google.cloud.dialogflow.cx.v3.UpdateIntentRequest request) {
       return blockingUnaryCall(getChannel(), getUpdateIntentMethod(), getCallOptions(), request);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Deletes the specified intent.
-     * </pre>
-     */
+    /** */
     public com.google.protobuf.Empty deleteIntent(
         com.google.cloud.dialogflow.cx.v3.DeleteIntentRequest request) {
       return blockingUnaryCall(getChannel(), getDeleteIntentMethod(), getCallOptions(), request);
     }
   }
 
-  /**
-   *
-   *
-   * <pre>
-   * Service for managing [Intents][google.cloud.dialogflow.cx.v3.Intent].
-   * </pre>
-   */
+  /** */
   public static final class IntentsFutureStub
       extends io.grpc.stub.AbstractFutureStub<IntentsFutureStub> {
     private IntentsFutureStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
@@ -609,13 +489,7 @@ public final class IntentsGrpc {
       return new IntentsFutureStub(channel, callOptions);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Returns the list of all intents in the specified agent.
-     * </pre>
-     */
+    /** */
     public com.google.common.util.concurrent.ListenableFuture<
             com.google.cloud.dialogflow.cx.v3.ListIntentsResponse>
         listIntents(com.google.cloud.dialogflow.cx.v3.ListIntentsRequest request) {
@@ -623,26 +497,14 @@ public final class IntentsGrpc {
           getChannel().newCall(getListIntentsMethod(), getCallOptions()), request);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Retrieves the specified intent.
-     * </pre>
-     */
+    /** */
     public com.google.common.util.concurrent.ListenableFuture<
             com.google.cloud.dialogflow.cx.v3.Intent>
         getIntent(com.google.cloud.dialogflow.cx.v3.GetIntentRequest request) {
       return futureUnaryCall(getChannel().newCall(getGetIntentMethod(), getCallOptions()), request);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Creates an intent in the specified agent.
-     * </pre>
-     */
+    /** */
     public com.google.common.util.concurrent.ListenableFuture<
             com.google.cloud.dialogflow.cx.v3.Intent>
         createIntent(com.google.cloud.dialogflow.cx.v3.CreateIntentRequest request) {
@@ -650,13 +512,7 @@ public final class IntentsGrpc {
           getChannel().newCall(getCreateIntentMethod(), getCallOptions()), request);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Updates the specified intent.
-     * </pre>
-     */
+    /** */
     public com.google.common.util.concurrent.ListenableFuture<
             com.google.cloud.dialogflow.cx.v3.Intent>
         updateIntent(com.google.cloud.dialogflow.cx.v3.UpdateIntentRequest request) {
@@ -664,13 +520,7 @@ public final class IntentsGrpc {
           getChannel().newCall(getUpdateIntentMethod(), getCallOptions()), request);
     }
 
-    /**
-     *
-     *
-     * <pre>
-     * Deletes the specified intent.
-     * </pre>
-     */
+    /** */
     public com.google.common.util.concurrent.ListenableFuture<com.google.protobuf.Empty>
         deleteIntent(com.google.cloud.dialogflow.cx.v3.DeleteIntentRequest request) {
       return futureUnaryCall(
