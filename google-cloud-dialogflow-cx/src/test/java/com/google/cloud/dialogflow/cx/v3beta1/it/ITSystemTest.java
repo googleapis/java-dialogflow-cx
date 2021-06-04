b794190290f033ec2d3b531ea995bacb8588a4f7
Mike Ganbold <munkhbayarg@google.com>
2021-06-04 11:02:08 -0600

    chore: skip TrainFlow IT for now

--- google-cloud-dialogflow-cx/src/test/java/com/google/cloud/dialogflow/cx/v3beta1/it/ITSystemTest.java
+++ google-cloud-dialogflow-cx/src/test/java/com/google/cloud/dialogflow/cx/v3beta1/it/ITSystemTest.java
@@ -89,6 +89,7 @@ import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ITSystemTest {
@@ -379,6 +380,7 @@ public class ITSystemTest {
   }
 
   @Test
+  @Ignore("https://github.com/googleapis/java-dialogflow-cx/issues/230")
   public void trainFlowTest() throws ExecutionException, InterruptedException {
     Empty expectedResponse = Empty.newBuilder().build();
     Empty response = flowsClient.trainFlowAsync(flowName).get();
