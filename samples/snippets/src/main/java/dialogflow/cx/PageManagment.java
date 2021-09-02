6e80e8fc38290bdd447e67753d940388e806cd1c
Gal Zahavi <galzahavi@google.com>
2021-09-02 15:12:49 -0700

    updated list tests

--- samples/snippets/src/main/java/dialogflow/cx/PageManagment.java
+++ samples/snippets/src/main/java/dialogflow/cx/PageManagment.java
@@ -71,8 +71,13 @@ public class PageManagment {
             + flowId);
     listRequestBuilder.setLanguageCode("en");
 
-    System.out.println(client.listPages(listRequestBuilder.build()));
-    
+    String result = ""
+
+    for (Page element : client.listPages(listRequestBuilder.build()).iterateAll()) {
+      result += element.toString() + "\n";
+    }
+
+    System.out.println(result)
   }
   // [END dialogflow_cx_list_pages]
 
