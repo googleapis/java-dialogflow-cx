5ba528855d580c1b1afb8b50eaa47f64ca11f58a
Gal Zahavi <galzahavi@google.com>
2021-09-20 15:29:53 -0700

    Added agent ids

--- samples/snippets/src/test/java/dialogflow/cx/ListTestCaseResultsIT.java
+++ samples/snippets/src/test/java/dialogflow/cx/ListTestCaseResultsIT.java
@@ -28,9 +28,9 @@ import org.junit.Test;
 public class ListTestCaseResultsIT {
 
   private static String PROJECT_ID = System.getenv().get("GOOGLE_CLOUD_PROJECT");
-  private static String agentId = "";
-  private static String testId = "";
-  private static String location = "";
+  private static String agentId = "1499b8e1-ab7d-43fd-9b08-30ee57194fc1";
+  private static String testId = "694a5447-6c40-4752-944e-e3e70580b273";
+  private static String location = "global";
   
   private ByteArrayOutputStream stdOut;
   private static PrintStream originalOut;
