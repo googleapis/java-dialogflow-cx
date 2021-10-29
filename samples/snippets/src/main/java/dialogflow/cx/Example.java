4d84459d4a1794be1bf35cb2068ba5bccc027a1a
Gal Zahavi <galzahavi@google.com>
2021-10-29 11:59:34 -0700

    lint fix

--- samples/snippets/src/main/java/dialogflow/cx/Example.java
+++ samples/snippets/src/main/java/dialogflow/cx/Example.java
@@ -27,10 +27,10 @@ import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.GsonBuilder;
 import java.io.BufferedWriter;
 
 public class Example implements HttpFunction {
