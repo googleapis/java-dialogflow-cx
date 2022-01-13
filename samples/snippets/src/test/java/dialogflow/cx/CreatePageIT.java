ce970d35f9072c373c21b88cbe501efbdf92f547
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2022-01-13 14:36:40 -0800

    lint fix

--- samples/snippets/src/test/java/dialogflow/cx/CreatePageIT.java
+++ samples/snippets/src/test/java/dialogflow/cx/CreatePageIT.java
@@ -56,7 +56,7 @@ public class CreatePageIT {
     if (newPageNameGlobal != null) {
       try (PagesClient pagesClient = PagesClient.create()) {
         pagesClient.deletePage(newPageNameGlobal);
-        pagesClient.shutdown()
+        pagesClient.shutdown();
       }
     }
 
@@ -68,7 +68,7 @@ public class CreatePageIT {
               .build();
       try (PagesClient pagesClient = PagesClient.create(pagesSettings)) {
         pagesClient.deletePage(newPageNameRegional);
-        pagesClient.shutdown()
+        pagesClient.shutdown();
       }
     }
   }
