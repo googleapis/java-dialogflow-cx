8052921a561bf3b24d77f5a832661cb3efd28ff3
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2022-02-02 15:58:47 -0800

    tried to solve nullExceptionPointer

--- samples/snippets/src/main/java/dialogflow/cx/CreatePage.java
+++ samples/snippets/src/main/java/dialogflow/cx/CreatePage.java
@@ -101,7 +101,7 @@ public class CreatePage {
 
       // Performs the create page request.
       Page response = pagesClient.createPage(parent, page);
-      System.out.format("Page created: %s\n", response);
+      System.out.format("Page created: %s\n", response.toString());
 
       return response;
     }
