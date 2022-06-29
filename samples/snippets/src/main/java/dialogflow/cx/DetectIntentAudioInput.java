7d4dad94998c52bb10b09872385619295afa2e01
aribray <45905583+aribray@users.noreply.github.com>
2022-06-29 15:45:05 -0500

    add detectintent with audio input sample and test
    Change-Id: I741c73d3f14bfe5f9b4ff6ec55d43f2687ac51dd

--- samples/snippets/src/main/java/dialogflow/cx/DetectIntentAudioInput.java
+++ samples/snippets/src/main/java/dialogflow/cx/DetectIntentAudioInput.java
@@ -16,42 +16,48 @@
 
 package dialogflow.cx;
 
-// [START dialogflow_cx_detect_intent_intent_input]
+// [START dialogflow_cx_detect_intent_audio_input]
 
 import com.google.api.gax.rpc.ApiException;
+import com.google.cloud.dialogflow.cx.v3beta1.AudioEncoding;
 import com.google.cloud.dialogflow.cx.v3beta1.DetectIntentRequest;
 import com.google.cloud.dialogflow.cx.v3beta1.DetectIntentResponse;
-import com.google.cloud.dialogflow.cx.v3beta1.IntentInput;
+import com.google.cloud.dialogflow.cx.v3beta1.AudioInput;
+import com.google.cloud.dialogflow.cx.v3beta1.InputAudioConfig;
 import com.google.cloud.dialogflow.cx.v3beta1.QueryInput;
 import com.google.cloud.dialogflow.cx.v3beta1.QueryResult;
 import com.google.cloud.dialogflow.cx.v3beta1.SessionName;
 import com.google.cloud.dialogflow.cx.v3beta1.SessionsClient;
 import com.google.cloud.dialogflow.cx.v3beta1.SessionsSettings;
 import com.google.common.collect.Maps;
+import com.google.protobuf.ByteString;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-public class DetectIntentIntentInput {
+public class DetectIntentAudioInput {
 
   // DialogFlow API Detect Intent sample with Intent input.
   public static void main(String[] args) throws IOException, ApiException {
     String projectId = "my-project-id";
     String locationId = "my-location-id";
     String agentId = "my-agent-id";
+    String audioFileName = "my-audio-file-name";
+    int sampleRateHertz = 16000;
     String sessionId = "my-session-id";
-    String intent = "my-intent-id";
     String languageCode = "my-language-code";
 
-    detectIntent(projectId, locationId, agentId, sessionId, intent, languageCode);
+    detectIntent(projectId, locationId, agentId, audioFileName, sampleRateHertz, sessionId, languageCode);
   }
 
   public static void detectIntent(
       String projectId,
       String locationId,
       String agentId,
+      String audioFileName,
+      int sampleRateHertz,
       String sessionId,
-      String intent,
       String languageCode)
       throws IOException, ApiException {
 
@@ -72,34 +78,65 @@ public class DetectIntentIntentInput {
 
       // TODO : Uncomment if you want to print session path
       // System.out.println("Session Path: " + session.toString());
+      InputAudioConfig inputAudioConfig = InputAudioConfig.newBuilder()
+        .setAudioEncoding(AudioEncoding.AUDIO_ENCODING_LINEAR_16)
+        .setSampleRateHertz(sampleRateHertz).build();
 
-      IntentInput.Builder intentInput = IntentInput.newBuilder().setIntent(intent);
+      try (FileInputStream audioStream = new FileInputStream(audioFileName)) {
+        // Subsequent requests must **only** contain the audio data.
+        // Following messages: audio chunks. We just read the file in fixed-size chunks. In reality
+        // you would split the user input by time.
+        byte[] buffer = new byte[4096];
+        int bytes = audioStream.read(buffer);
+        AudioInput audioInput =
+        AudioInput.newBuilder().setAudio(ByteString.copyFrom(buffer, 0, bytes))
+        .setConfig(inputAudioConfig)
+        .build();
+        QueryInput queryInput =
+        QueryInput.newBuilder()
+            .setAudio(audioInput)
+            .setLanguageCode("en-US") // languageCode = "en-US"
+            .build();
 
-      // Build the query with the IntentInput and language code (en-US).
-      QueryInput queryInput =
-          QueryInput.newBuilder().setIntent(intentInput).setLanguageCode(languageCode).build();
+        DetectIntentRequest request =
+        DetectIntentRequest.newBuilder()
+            .setSession(session.toString())
+            .setQueryInput(queryInput)
+            .build();
+    
+        // Performs the detect intent request.
+        DetectIntentResponse response = sessionsClient.detectIntent(request);
+
+        // Display the query result.
+        QueryResult queryResult = response.getQueryResult();
+
+        System.out.println("====================");
+        System.out.format("Detected Intent: %s (confidence: %f)\n",
+        queryResult.getTranscript(),
+        queryResult.getIntentDetectionConfidence());
+        }
+
+       
+
+      }
+
+    //   AudioInput audioInput = AudioInput.newBuilder()
+    //     .setConfig(inputAudioConfig)
+    //     .setAudio(audioFileName)
+    //     .build();
+
+      // Build the query with the AudioInput and language code (en-US).
+    //   QueryInput queryInput =
+    //       QueryInput.newBuilder().setAudio(audioInput).setLanguageCode(languageCode).build();
 
       // Build the DetectIntentRequest with the SessionName and QueryInput.
-      DetectIntentRequest request =
-           DetectIntentRequest.newBuilder()
-          .setSession(session.toString())
-          .setQueryInput(queryInput)
-          .build();
+      
 
-      // Performs the detect intent request.
-      DetectIntentResponse response = sessionsClient.detectIntent(request);
-
-      // Display the query result.
-      QueryResult queryResult = response.getQueryResult();
+      
 
       // TODO : Uncomment if you want to print queryResult
-      System.out.println("====================");
-      System.out.format(
-          "Detected Intent: %s (confidence: %f)\n",
-          queryResult.getIntent().getDisplayName(),
-          queryResult.getIntentDetectionConfidence());
-    }
+      
   }
 }
 
-// [END dialogflow_cx_detect_intent_intent_input]
+// [END dialogflow_cx_detect_intent_audio_input]
