6e95970d2d28219aeac28ba59d12948371c4dea9
aribray <45905583+aribray@users.noreply.github.com>
2022-06-29 16:17:53 -0500

    add detectintent with event input sample and test
    Change-Id: Ie0846bd780f20cd1a8406f8a1278ccf45824b646

---
+++ samples/snippets/src/main/java/dialogflow/cx/DetectIntentEventInput.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dialogflow.cx;
+
+// [START dialogflow_cx_detect_intent_event_input]
+
+import com.google.api.gax.rpc.ApiException;
+import com.google.cloud.dialogflow.cx.v3beta1.DetectIntentRequest;
+import com.google.cloud.dialogflow.cx.v3beta1.DetectIntentResponse;
+import com.google.cloud.dialogflow.cx.v3beta1.EventInput;
+import com.google.cloud.dialogflow.cx.v3beta1.QueryInput;
+import com.google.cloud.dialogflow.cx.v3beta1.QueryResult;
+import com.google.cloud.dialogflow.cx.v3beta1.SessionName;
+import com.google.cloud.dialogflow.cx.v3beta1.SessionsClient;
+import com.google.cloud.dialogflow.cx.v3beta1.SessionsSettings;
+import com.google.common.collect.Maps;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class DetectIntentEventInput {
+
+  // DialogFlow API Detect Intent sample with Event input.
+  public static void main(String[] args) throws IOException, ApiException {
+    String projectId = "my-project-id";
+    String locationId = "my-location-id";
+    String agentId = "my-agent-id";
+    String sessionId = "my-session-id";
+    String event = "my-event-id";
+    String languageCode = "my-language-code";
+
+    detectIntent(projectId, locationId, agentId, sessionId, event, languageCode);
+  }
+
+  public static void detectIntent(
+      String projectId,
+      String locationId,
+      String agentId,
+      String sessionId,
+      String event,
+      String languageCode)
+      throws IOException, ApiException {
+
+    SessionsSettings.Builder sessionsSettingsBuilder = SessionsSettings.newBuilder();
+    if (locationId.equals("global")) {
+      sessionsSettingsBuilder.setEndpoint("dialogflow.googleapis.com:443");
+    } else {
+      sessionsSettingsBuilder.setEndpoint(locationId + "-dialogflow.googleapis.com:443");
+    }
+    SessionsSettings sessionsSettings = sessionsSettingsBuilder.build();
+
+    // Instantiates a client
+    try (SessionsClient sessionsClient = SessionsClient.create(sessionsSettings)) {
+      // Set the session name using the projectID (my-project-id), locationID (global), agentID
+      // (UUID), and sessionId (UUID).
+      SessionName session =
+          SessionName.ofProjectLocationAgentSessionName(projectId, locationId, agentId, sessionId);
+
+      // TODO : Uncomment if you want to print session path
+      // System.out.println("Session Path: " + session.toString());
+
+      EventInput.Builder eventInput = EventInput.newBuilder().setEvent(event);
+
+      // Build the query with the EventInput and language code (en-US).
+      QueryInput queryInput =
+          QueryInput.newBuilder().setEvent(eventInput).setLanguageCode(languageCode).build();
+
+      // Build the DetectIntentRequest with the SessionName and QueryInput.
+      DetectIntentRequest request =
+           DetectIntentRequest.newBuilder()
+          .setSession(session.toString())
+          .setQueryInput(queryInput)
+          .build();
+
+      // Performs the detect intent request.
+      DetectIntentResponse response = sessionsClient.detectIntent(request);
+
+      // Display the query result.
+      QueryResult queryResult = response.getQueryResult();
+
+      // TODO : Uncomment if you want to print queryResult
+      System.out.println("====================");
+      System.out.format(
+          "Triggering Event: %s \n",
+          queryResult.getTriggerEvent());
+    }
+  }
+}
+
+// [END dialogflow_cx_detect_intent_event_input]
