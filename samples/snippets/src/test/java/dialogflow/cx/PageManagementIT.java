a5d2cac76777cbe91bb11963e9c643ce73697cde
aribray <45905583+aribray@users.noreply.github.com>
2022-07-18 11:45:01 -0500

    remove try block
    Change-Id: I9b5f96d7c0ee75745084ed2a277fb9dc8b76a98f

--- samples/snippets/src/test/java/dialogflow/cx/PageManagementIT.java
+++ samples/snippets/src/test/java/dialogflow/cx/PageManagementIT.java
@@ -84,9 +84,9 @@ public class PageManagementIT {
 
   @Test
   public void testCreatePage() throws IOException {
-    try (Page p = CreateSimplePage.createPage(PROJECT_ID, agentID, flowID, location, displayName)) {
-      pageID = p.getName().split("/")[9];
-    }
+    Page p = CreateSimplePage.createPage(PROJECT_ID, agentID, flowID, location, displayName);
+    pageID = p.getName().split("/")[9];
+
     assertThat(p.getDisplayName()).isEqualTo(displayName);
   }
 
